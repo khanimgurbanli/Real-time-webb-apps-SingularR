c3a91c4fa2381fe72e53b01f0f0ed477e7cb10bc
khanimgurbanli <zeri.q.222@gmail.com>
2022-09-29 15:05:03 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using SignalRChat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(options =>
+    {
+        options.AllowAnyHeader()
+
+                .AllowCredentials()
+                .AllowAnyMethod()
+                .SetIsOriginAllowed(origin => true);
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
